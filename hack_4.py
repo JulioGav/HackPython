adb2660f76363c478fb3ef7e0e09eb694d326685
JulioGav <159968062+JulioGav@users.noreply.github.com>
2024-05-15 16:14:14 -0400

    hack_4.py

--- hack_4.py
+++
@@ -1,9 +0,0 @@
-"""
-text: "fooziman" output => "foozimaN"
-"""
-
-def fn_hack_4():
-    palabra = "fooziman"
-    # Reemplaza la última letra con una 'N'
-    resultado = "foozimaN"[:-1] + "N"
-    return resultado
